a037cac8b651c9f3d60201a9dfccfc49d3db6ee2
Joaquin <joaquin.telleria@wh-d.at>
2025-05-27 14:53:52 +0200

    initial commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm flex-col">
-        <h1 className="text-4xl font-bold">Main</h1>
-        <p className="mt-4">This is the main page.</p>
+        <h1 className="text-4xl font-bold">About</h1>
+        <p className="mt-4">This is the about page.</p>
       </div>
     </main>
   );
