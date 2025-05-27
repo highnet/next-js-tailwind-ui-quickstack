a48cdbae73a133712581e0d68325c72804e4e8ca
Joaquin <joaquin.telleria@wh-d.at>
2025-05-27 15:43:14 +0200

    feat: add utility function for conditional class names using clsx and tailwind-merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,35 @@
 "use client";
-import Button from "@/components/Button";
+
+import { Button } from "@/components/Button";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm flex-col">
         <h1 className="text-4xl font-bold">Main</h1>
-        <p className="mt-4">This is the main page.</p>
-        <Button
-          className="mt-2"
-          onClick={() => alert("Action Button Clicked!")}
-        >
-          Click Me
-        </Button>
+        <p className="mt-2">This is the main page.</p>
+        <div className="mt-4 flex gap-2">
+          <Button
+            className="mt-2"
+            onClick={() => window.open("https://nextjs.org", "_blank")}
+          >
+            Next.js
+          </Button>
+          <Button
+            className="mt-2"
+            onClick={() => window.open("https://tailwindcss.com", "_blank")}
+            variant="secondary"
+          >
+            Tailwind CSS
+          </Button>
+          <Button
+            className="mt-2"
+            onClick={() => window.open("https://mui.com/base-ui/", "_blank")}
+            variant="outline"
+          >
+            Base UI
+          </Button>{" "}
+        </div>
       </div>
     </main>
   );
